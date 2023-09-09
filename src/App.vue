864f01b2b4ad2ebb1eaac5da832118947f7d271e
Mateus Oliveira <teussharke@gmail.com>
2023-09-09 09:05:38 -0300

    First Commit
    Projeto Stemis

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="https://media.licdn.com/dms/image/C4D0BAQGTyfIGl0yJHw/company-logo_200_200/0/1663624441444?e=1702512000&v=beta&t=rI5uSkikngc6v8R5W60xuBDogvrg2kPMNNnwK6LQrFM" width="125" height="125" />
+
+
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+header{
+  display: flex;
+  justify-content: center;
+  padding: 2rem;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,168 @@
+<template>
+<div class="geral">
+    <div class="primeira">
+
+    <form class="form">
+    
+    <h1> Cadastrar </h1>
+
+    
+    <label>
+        <span> Seu Email</span>
+        <input type="email" name="email">
+     </label>
+
+      <label>
+        <span> Senha</span>
+        <input type="password" name="password">
+     </label>
+
+    <button class="submit" type="button"> Cadastrar - Clique Aqui</button>
+
+     
+
+</form>     
+
+</div>
+
+
+<div class="segunda">
+
+
+<form class="form">
+    
+    <h1> Logar </h1>
+
+    
+    <label>
+
+    <span> Seu Email</span>
+        <input type="email" name="email">
+     </label>
+
+      <label>
+        <span> Senha</span>
+        <input type="password" name="password">
+     </label>
+
+    <button class="submit" type="button">Logar - Clique Aqui</button>
+
+     
+
+</form>  
+
+
+</div>
+</div>
+ 
+
+
+
+
+   
+</template>
+
+
+<script>
+
+
+</script>
+
+
+<style scoped>
+
+
+    .geral {
+        display: flex;
+        justify-content: center;
+    }
+    .primeira {
+
+        
+        background: #fff;
+        box-shadow: 0 19px 38px black, 0 15px 12px rgba(0, 0, 0, 0.22);
+        border-radius: 25px;
+        width: 40%;        
+           
+    }
+
+    .segunda{
+
+       
+        background: #fff;
+        box-shadow: 0 19px 38px black, 0 15px 12px rgba(0, 0, 0, 0.22);
+        border-radius: 25px;
+        width: 40%;
+        margin-left: 30px;
+       
+    }
+
+    input, button{
+        border:none;
+        outline: none;
+        background: none;
+    }
+
+    h1 {
+        color: black;
+        padding-top: 10px;
+        font-size: 30px;
+       
+    }
+
+
+
+    .form{
+        
+        padding: 50px 30px;
+        -webkit-transition:-webkit-transform 1.2s ease-in-out;
+        transition: -webkit-transform 1.2s ease-in-out;
+        transition: transform 1.2s ease-in-out;
+        transition: transform 1.2s ease-in-out, -webkit-transform 1.2s ease-in-out;
+    }
+
+    label{
+        display: block;
+        width: 260px;
+        margin: 25px auto 0;
+        text-align: center;
+    }
+
+    label span{
+        font-size: 14px;
+        font-weight: 600;
+        color: #505f75;
+        text-transform: uppercase;
+    }
+
+    input{
+        display: block;
+        width: 100%;
+        margin-top: 5px;
+        font-size: 16px;
+        padding-bottom: 5px;
+        border-bottom: 1px solid rgba(109, 93, 93, 0.4);
+        text-align: center;
+        font-family: 'Nunito', sans-serif; 
+    }
+
+    button{
+        display: block;
+        margin: 20px  auto;
+
+        width: 260px;
+        height: 36px;
+        border-radius: 30px;
+        background-color: blue;
+        color: white;
+        font-size: 15px;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background-color: red;
+    }
+
+
+   
+</style>
